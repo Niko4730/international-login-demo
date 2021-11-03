26cc9fa69d642e12bd1183e7cd6dcbed9df92add
Niko4730 <niko4730@easv365.dk>
2021-11-03 09:41:36 +0100

    products plumming
    structure and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'products',
+    loadChildren: () => import ('./products/products.module')
+      .then(m => m.ProductsModule)
+  },
+  {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module')
       .then(m => m.AuthModule)
